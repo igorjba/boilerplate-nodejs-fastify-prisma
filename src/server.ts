c949032da92ac35b946a086dc8d48374592f4c6e
Igor Bahia <igorjbahia@gmail.com>
2023-09-16 11:02:32 -0300

    :tada: first commit.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import { app } from './app'
 import { env } from './env'
 
+app.get('/', async (request, reply) => {
+  return { message: 'Hello, World!' };
+});
+
 app
   .listen({
     host: '0.0.0.0',
